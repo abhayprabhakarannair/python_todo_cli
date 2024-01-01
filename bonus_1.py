89e90f7dccd59f14b9b050986cb14d7023b206c1
Abhay P Nair <abhayprabhakarannair@gmail.com>
2024-01-01 21:56:07 +0530

    Add new functionalities and .gitignore file
    Created three new python scripts: bonus_1.py, .gitignore, and main.py. bonus_1.py script is designed to take user input (name) and print its length, while main.py script takes down a todo list from user input and print it out with its type. The .gitignore file was also added to exclude specific directories from Git tracking.

---
+++ bonus_1.py
@@ -0,0 +1,5 @@
+# Get user input
+user_input = input("Enter your name: ")
+
+# Print its length
+print("The length of your name is ", len(user_input))
---
+++ main.py
@@ -0,0 +1,14 @@
+# Get inputs from user
+todo1 = input("Enter a todo: ")
+todo2 = input("Enter a todo: ")
+todo3 = input("Enter a todo: ")
+
+# Declare a list of todos
+todo_list = [todo1, todo2, todo3]
+
+# Print those
+print(todo_list)
+
+# See the type difference of the todo1 and the todo_list
+print(type(todo1))
+print(type(todo_list))
