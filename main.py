658605c37ab227088f6dc96bf539b0d585f08eb9
Abhay P Nair <abhayprabhakarannair@gmail.com>
2024-01-03 19:12:46 +0530

    Add function to output ordinal list of todo items
    The commit introduces a new function 'get_ordinal' to convert item numbers to their ordinal forms (1st, 2nd, 3rd, etc) for improved readability. This function is utilized in a new enumeration loop that prints the todo items along with their ordinal position in the todo list.

--- main.py
+++ main.py
@@ -15,7 +15,27 @@ while count <= 5:
     # Print list data
     print(todo_list)
     # Increment count
-    count = count+1
+    count = count + 1
 
 # Should have only 5 items
 print("You entered " + str(len(todo_list)) + " todo items")
+
+def get_ordinal(number: int):
+    ordinal_val: str
+    if number == 1:
+        ordinal_val = str(1) + 'st'
+    elif number == 2:
+        ordinal_val = str(2) + 'nd'
+    elif number == 3:
+        ordinal_val = str(3) + 'rd'
+    else:
+        ordinal_val = str(number) + 'th'
+
+    return ordinal_val
+
+
+# Should out put all items one by one.
+for index, todo in enumerate(todo_list):
+    print("The " + get_ordinal(index + 1) + " todo item is " + todo)
+
+
